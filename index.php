8e7c0110a7787d9e6f0c5a9617fe7efbab6d5675
Kei Omizo <komizo@redhat.com>
2016-11-17 17:43:51 +0900

    update

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo "<h1>Hello OpenShift<h1>";
-echo "Container Name";
+echo "Container Name: ";
 echo gethostname();
-
-echo "Client IP";
+echo "<br>";
+echo "Client IP: ";
 echo $_SERVER["REMOTE_ADDR"];
 ?>
