fe1c999aa7d1baa8ec3f3c24d50a4d10d1426f2f
Kei Omizo <komizo@redhat.com>
2016-11-27 17:27:15 +0900

    update

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 echo "<h1>Hello OpenShift<h1>";
+echo "Greetings:";
+echo getenv("GREETINGS");
+
+<br>
+
 echo "Container Name: ";
 echo gethostname();
 echo "<br>";
 echo "Client IP: ";
 echo $_SERVER["REMOTE_ADDR"];
-echo "2016-11-27";
 ?>
